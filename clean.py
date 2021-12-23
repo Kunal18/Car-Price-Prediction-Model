6b03ab71e4701d19957828eb9e6fd7a786682003
KUNAL INDORE <kunalindore18@gmail.com>
2021-12-24 00:18:21 +0530

    Create clean.py

---
+++ clean.py
@@ -0,0 +1,27 @@
+#Splitting company name from CarName column
+CompanyName = cars['CarName'].apply(lambda x : x.split(' ')[0])
+cars.insert(3,"CompanyName",CompanyName)
+cars.drop(['CarName'],axis=1,inplace=True)
+cars.head()
+
+
+cars.CompanyName.unique()
+
+
+cars.CompanyName = cars.CompanyName.str.lower()
+
+def replace_name(a,b):
+    cars.CompanyName.replace(a,b,inplace=True)
+
+replace_name('maxda','mazda')
+replace_name('porcshce','porsche')
+replace_name('toyouta','toyota')
+replace_name('vokswagen','volkswagen')
+replace_name('vw','volkswagen')
+
+cars.CompanyName.unique()
+
+#Checking for duplicates
+cars.loc[cars.duplicated()]
+
+cars.columns

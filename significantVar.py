4c13d443b19dceada8af7090abe4bc3243530bf3
KUNAL INDORE <kunalindore18@gmail.com>
2021-12-24 00:26:04 +0530

    Create significantVar.py

---
+++ significantVar.py
@@ -0,0 +1,28 @@
+cars_lr = cars[['price', 'fueltype', 'aspiration','carbody', 'drivewheel','wheelbase',
+                  'curbweight', 'enginetype', 'cylindernumber', 'enginesize', 'boreratio','horsepower', 
+                    'fueleconomy', 'carlength','carwidth', 'carsrange']]
+cars_lr.head()
+
+sns.pairplot(cars_lr)
+plt.show()
+
+#Dummy Var
+# Defining the map function
+def dummies(x,df):
+    temp = pd.get_dummies(df[x], drop_first = True)
+    df = pd.concat([df, temp], axis = 1)
+    df.drop([x], axis = 1, inplace = True)
+    return df
+# Applying the function to the cars_lr
+
+cars_lr = dummies('fueltype',cars_lr)
+cars_lr = dummies('aspiration',cars_lr)
+cars_lr = dummies('carbody',cars_lr)
+cars_lr = dummies('drivewheel',cars_lr)
+cars_lr = dummies('enginetype',cars_lr)
+cars_lr = dummies('cylindernumber',cars_lr)
+cars_lr = dummies('carsrange',cars_lr)
+
+
+cars_lr.head()
+cars_lr.shape
